52c13bab5aad1aeafde5f93063e2bff247d6254c
mcleodheads <vamaklakov@mmtr.ru>
2021-11-01 09:43:24 +0300

    True initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,10 @@
 import React from 'react';
 
-import './assets/styles/style.scss'
-
 const App = () => {
     return (
-        <div className={'wrapper'}>
-            <div className={'word'}>
-                yoda ->&nbsp;
-            </div>
-            <div className={'image'}></div>
-        </div>
+        <div>
 
+        </div>
     );
 };
 
